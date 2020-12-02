a3e742150601127c06e9a54cf41b1677825bb099
Wellington <w_santos1993@outlook.com>
2020-12-02 11:04:15 -0300

    criando rota de login

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -3,12 +3,12 @@ const express = require('express');
 
 //Criar o roteador [express.router()]
 const router = express.Router();
+// importando o Authcontroller
+const AuthController = require('../controllers/AuthController');
 
 //fazer com que o roteador responda qualquer coisa para as req
 //do tipo post para  o endereco /login.
-router.post('/login', (req, res) => {
-    res.send("olá")
-});
+router.post('/login', AuthController.index);
 
 //exportar o roteador
 module.exports = router;
